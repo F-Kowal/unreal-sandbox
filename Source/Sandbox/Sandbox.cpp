3667fb1987f8aa4127631436c6d136c7bb95ad57
F-Kowal <fkowal.krk@gmail.com>
2026-08-22 15:48:36 +0200

    restructure project and add assets

--- Source/Sandbox/Sandbox.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Sandbox.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sandbox, "Sandbox" );
